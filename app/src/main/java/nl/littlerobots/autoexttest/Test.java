fb005fc1327347d66311a6e06f02c971f9b58bb6
Ryan Harter <ryanjharter@gmail.com>
2015-05-15 15:13:36 -0500

    Adds more features to the test class.

--- app/src/main/java/nl/littlerobots/autoexttest/Test.java
+++ app/src/main/java/nl/littlerobots/autoexttest/Test.java
@@ -7,4 +7,20 @@ import com.google.auto.value.AutoValue;
 @AutoValue
 public abstract class Test implements Parcelable {
     public abstract String name();
+    public abstract long pushups();
+    public abstract long time();
+
+    public double pushupsOverTime() {
+      return pushups() / time();
+    }
+    
+    public abstract Builder toBuilder();
+
+    @AutoValue.Builder
+    public interface Builder {
+      Builder name(String name);
+      Builder pushups(long pushups);
+      Builder time(long time);
+      Test build();
+    }
 }
